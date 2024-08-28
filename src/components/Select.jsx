07b1cccde39221f11d6c6d7c8466a9b1842d3492
0zodbek <ozpdbekov@gmail.com>
2024-08-28 13:14:29 +0500

    select add

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -1,32 +1,63 @@
-import React, { useEffect, useState } from 'react'
-
-function Select({countries}) {
-    const [openOptions, setOpenOptions] = useState(false)
-    const [selected, setSelected] = useState({})
-    useEffect(()=>{
-    setSelected(countries[0])
-    },[])
+import React, { useEffect, useState } from "react";
+import countriesData from "../assets/country-currency.json"
+function Select({ countries }) {
+  const [openOptions, setOpenOptions] = useState(false);
+  const [selected, setSelected] = useState({});
+  useEffect(() => {
+    setSelected(countries[0]);
+  }, []);
+  function handleSelectCountry(country){
+ setSelected(country)
+ setOpenOptions(false)
+}
+  console.log(openOptions);
   return (
     <div>
-        <label htmlFor="select">To</label>
-        <div onClick={()=>{setOpenOptions(true)}}  className="border w-72 flex  p-3 rounded justify-between cursor-pointer">
-        <img src={selected.flag} alt={`${selected.name}flag`} width={20}/>
-                    { selected.currencies && <span>{Object.keys(selected.currencies)} - {Object.values(selected.currencies)[0].name}</span> }
-            {/* <input type="text"  id="select" placeholder='Type to search' /> */}
-            { openOptions && <span><i class="fa-solid fa-chevron-down"></i></span> }
-        </div>
-        <div >
-            { openOptions && <ul className="options shadow-lg px-3 w-72 overflow-scroll h-80 overflow-x-hidden rounded">
-                { countries && countries.map(function(country,index){
-                return <li key={index} className='flex gap-5 mt-2 items-center cursor-pointer hover:bg-slate-300 text-sm'>
-                    <img src={country.flag} alt={`${country.name}flag`} width={20}/>
-                    { country.currencies && <span>{Object.keys(country?.currencies)} - {Object.values(country?.currencies)[0].name}</span> }
-                </li>
-            })}
-                </ul>}
-        </div>
+    <p className='mt-[-36px] mb-2 text-lg font-bold'>To</p>
+    <div 
+      onClick={() => setOpenOptions(!openOptions)} 
+      className="border-2 w-80 flex p-3 h-12 rounded justify-between cursor-pointer"
+    >
+      {selected && (
+        <>
+          <img src={selected.flag} alt={`${selected.name} flag`} className='w-8' />
+          <span>
+            {selected.currencies && (
+              `${Object.keys(selected.currencies)[0]} - ${Object.values(selected.currencies)[0].name}`
+            )}
+          </span>
+        </>
+      )}
+      <span>
+        {!openOptions ? (
+          <i className="fa-solid fa-chevron-down"></i>
+        ) : (
+          <i onClick={() => setOpenOptions(false)} className="fa-solid fa-xmark"></i>
+        )}
+      </span>
     </div>
-  )
+
+    {openOptions && (
+      <ul className="options shadow-lg px-3 w-72 mt-2 bg-white overflow-y-scroll h-80 rounded">
+        {countriesData.map((country, index) => (
+          <li 
+            key={index} 
+            className="flex gap-5 mt-2 items-center cursor-pointer hover:bg-slate-300 text-sm"
+            onClick={() => handleSelectCountry(country)}
+          >
+            <img src={country.flag} alt={`${country.name} flag`} className='w-8 h-6' />
+            <span>
+              {country.currencies && (
+                `${Object.keys(country.currencies)[0]} - ${Object.values(country.currencies)[0].name}`
+              )}
+            </span>
+          </li>
+        ))}
+      </ul>
+    )}
+  </div>
+
+  );
 }
 
-export default Select
+export default Select;
