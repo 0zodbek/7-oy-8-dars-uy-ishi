20dfddb5eb89d005eb1897d8f04238332b3f72fa
0zodbek <ozpdbekov@gmail.com>
2024-08-28 16:53:32 +0500

    project

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -1,67 +1,182 @@
-import React, { useEffect, useState } from "react";
+import React, {useState,useEffect} from 'react'
 import countriesData from "../assets/country-currency.json"
-function Select({ countries }) {
-  const [openOptions, setOpenOptions] = useState(false);
-  const [selected, setSelected] = useState({});
+import "../App.css"
+function Select() {
+  const [openFromOptions, setOpenFromOptions] = useState(false);
+  const [openToOptions, setOpenToOptions] = useState(false);
+  const [selectedFrom, setSelectedFrom] = useState(null);
+  const [selectedTo, setSelectedTo] = useState(null);
+  const [amount, setAmount] = useState('');
+  const [result, setResult] = useState(null);
+  const [searchFrom, setSearchFrom] = useState('');
+  const [searchTo, setSearchTo] = useState('');
+
   useEffect(() => {
-    setSelected(countries[0]);
+    if (countriesData.length > 0) {
+      setSelectedFrom(countriesData[0]);
+      setSelectedTo(countriesData[0]);
+    }
   }, []);
-  function handleSelectCountry(country){
- setSelected(country)
- setOpenOptions(false)
-}
-  console.log("openoptions",openOptions);
+
+  function handleSelectCountry(country, type) {
+    if (type === 'from') {
+      setSelectedFrom(country);
+      setOpenFromOptions(false);
+    } else {
+      setSelectedTo(country);
+      setOpenToOptions(false);
+    }
+  }
+
+  function handleConvert() {
+    if (selectedFrom && selectedTo && amount) {
+      const fromRate = Object.values(selectedFrom.currencies)[0].rateToUSD;
+      const toRate = Object.values(selectedTo.currencies)[0].rateToUSD;
+      const Amount = (fromRate / toRate) * amount;
+      setResult(Amount);
+    }
+  }
+
+  const filteredFromCountries = countriesData.filter(country =>
+    country.name.toLowerCase().includes(searchFrom.toLowerCase())
+  );
+
+  const filteredToCountries = countriesData.filter(country =>
+    country.name.toLowerCase().includes(searchTo.toLowerCase())
+  );
+
   return (
-    <div>
-    <p className='mt-[-36px] mb-2 text-lg font-bold'>To</p>
-    <div  
-      className="border-1 w-80 flex p-3 h-14 border border-slate-300 rounded justify-between cursor-pointer selector" onClick={(e)=>{
-        console.log(e.target);
-    }}
-    >
-      { !openOptions && (
-        <>
-          <div onClick={() => setOpenOptions(!openOptions)} className=" flex items-center gap-5">
-          <img src={selected.flag} alt={`${selected.name} flag`} className='w-8' />
+    <div className="flex justify-between mt-16 px-16">
+      <p className='absolute top-40 font-bold'>Amount</p>
+      <input
+        type="number"
+        placeholder="Amount to convert..."
+        className="input input-bordered w-72 p-3 max-h-12 bg-slate-50 rounded-md max-w-xs border-2"
+        value={amount}
+        onChange={(e) => setAmount(e.target.value)}
+      />
+      
+      <div>
+        <p className="mt-[-36px] mb-2 text-lg font-bold">From</p>
+        <div
+          className="border-2 w-80 flex p-3 h-12 rounded justify-between cursor-pointer"
+        >
+          {selectedFrom && !openFromOptions && (
+            <div onClick={() => setOpenFromOptions(!openFromOptions)} className='flex items-center gap-4'>
+              <img src={selectedFrom.flag} alt={`${selectedFrom.name} flag`} className="w-8" />
+              <span>
+                {selectedFrom.currencies && (
+                  `${Object.keys(selectedFrom.currencies)[0]} - ${Object.values(selectedFrom.currencies)[0].name}`
+                )}
+              </span>
+            </div>
+          )}
+          {openFromOptions && (
+            <label className="input input-bordered flex items-center gap-2">
+              <input
+                type="text"
+                className="grow outline-none bg-slate-50"
+                placeholder="Search"
+                value={searchFrom}
+                onChange={(e) => setSearchFrom(e.target.value)}
+              />
+            </label>
+          )}
           <span>
-            {selected.currencies && (
-              `${Object.keys(selected.currencies)[0]} - ${Object.values(selected.currencies)[0].name}`
+            {!openFromOptions ? (
+              <i className="fa-solid fa-chevron-down"></i>
+            ) : (
+              <i onClick={() => setOpenFromOptions(false)} className="fa-solid fa-xmark"></i>
             )}
           </span>
-          </div>
-        </>
-      )}
-      { openOptions && <input className="outline-none w-60 bg-transparent h-6 z-1000 focus-within:" placeholder="Type to search"></input>}
-      <span>
-        {!openOptions ? (
-          <i onClick={() => setOpenOptions(!openOptions)} className="fa-solid fa-chevron-down"></i>
-        ) : (
-          <i onClick={() => setOpenOptions(false)} className="fa-solid fa-xmark"></i>
+        </div>
+
+        {openFromOptions && (
+          <ul className="options shadow-lg px-3 w-72 bg-white overflow-y-scroll h-80 rounded">
+            {filteredFromCountries.map((country, index) => (
+              <li
+                key={index}
+                className="flex gap-5 mt-2 items-center cursor-pointer hover:bg-slate-300 text-sm"
+                onClick={() => handleSelectCountry(country, 'from')}
+              >
+                <img src={country.flag} alt={`${country.name} flag`} className="w-8 h-6" />
+                <span>
+                  {country.currencies && (
+                    `${Object.keys(country.currencies)[0]} - ${Object.values(country.currencies)[0].name}`
+                  )}
+                </span>
+              </li>
+            ))}
+          </ul>
         )}
-      </span>
+      </div>
+
+
+      <div>
+        <p className="mt-[-36px] mb-2 text-lg font-bold">To</p>
+        <div
+          className="border-2 w-80 flex p-3 h-12 rounded justify-between cursor-pointer"
+        >
+          {selectedTo && !openToOptions && (
+            <div onClick={() => setOpenToOptions(!openToOptions)} className='flex items-center gap-4'>
+              <img src={selectedTo.flag} alt={`${selectedTo.name} flag`} className="w-8" />
+              <span>
+                {selectedTo.currencies && (
+                  `${Object.keys(selectedTo.currencies)[0]} - ${Object.values(selectedTo.currencies)[0].name}`
+                )}
+              </span>
+            </div>
+          )}
+          {openToOptions && (
+            <label className="input input-bordered flex items-center gap-2">
+              <input
+                type="text"
+                className="grow outline-none bg-slate-50"
+                placeholder="Search"
+                value={searchTo}
+                onChange={(e) => setSearchTo(e.target.value)}
+              />
+            </label>
+          )}
+          <span>
+            {!openToOptions ? (
+              <i className="fa-solid fa-chevron-down"></i>
+            ) : (
+              <i onClick={() => setOpenToOptions(false)} className="fa-solid fa-xmark"></i>
+            )}
+          </span>
+        </div>
+
+        {openToOptions && (
+          <ul className="options shadow-lg px-3 w-72 bg-white overflow-y-scroll h-80 rounded">
+            {filteredToCountries.map((country, index) => (
+              <li
+                key={index}
+                className="flex gap-5 mt-2 items-center cursor-pointer hover:bg-slate-300 text-sm"
+                onClick={() => handleSelectCountry(country, 'to')}
+              >
+                <img src={country.flag} alt={`${country.name} flag`} className="w-8 h-6" />
+                <span>
+                  {country.currencies && (
+                    `${Object.keys(country.currencies)[0]} - ${Object.values(country.currencies)[0].name}`
+                  )}
+                </span>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+      <button onClick={handleConvert} className="convert absolute top-[480px] right-[260px] bg-white font-semibold border-2 p-4 rounded-md">
+        Convert
+      </button>
+
+      {result && (
+        <div className="mt-4">
+          <p className="text-lg font-bold absolute fixed top-[350px] left-[290px]">Amount: {result}</p>
+        </div>
+      )}
     </div>
-
-    {openOptions && (
-      <ul className="options shadow-lg px-3 w-72 mt-2 bg-white overflow-y-scroll h-80 rounded">
-        {countriesData.map((country, index) => (
-          <li 
-            key={index} 
-            className="flex gap-5 mt-2 items-center cursor-pointer hover:bg-slate-300 text-sm"
-            onClick={() => handleSelectCountry(country)}
-          >
-            <img src={country.flag} alt={`${country.name} flag`} className='w-8 h-6' />
-            <span>
-              {country.currencies && (
-                `${Object.keys(country.currencies)[0]} - ${Object.values(country.currencies)[0].name}`
-              )}
-            </span>
-          </li>
-        ))}
-      </ul>
-    )}
-  </div>
-
   );
 }
 
-export default Select;
+export default Select
